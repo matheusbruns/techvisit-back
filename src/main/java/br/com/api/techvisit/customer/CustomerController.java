b409b9b69fdf0081c5e4ab18c6b00a5140522f6c
matheusbruns <matheus.rbruns@gmail.com>
2024-08-29 00:46:45 -0300

    TECH-1 feat: adicionado endpoints GET e POST de clientes

--- src/main/java/br/com/api/techvisit/customer/CustomerController.java
+++ src/main/java/br/com/api/techvisit/customer/CustomerController.java
@@ -1,11 +1,19 @@
 package br.com.api.techvisit.customer;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.techvisit.customer.bean.CustomerBean;
 
 @RestController
 @RequestMapping("/customer")
@@ -15,8 +23,14 @@ public class CustomerController {
 	private CustomerService customerService;
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public String getAllCustomers(@RequestParam("organization") Long organizationId) {
+	public List<CustomerBean> getAllCustomers(@RequestParam("organization") Long organizationId) {
 		return this.customerService.getAllByOrganization(organizationId);
+	}
+
+	@PostMapping()
+	@ResponseStatus(HttpStatus.CREATED)
+	public CustomerBean save(@RequestBody CustomerBean customerBean) {
+		return this.customerService.save(customerBean);
 	}
 
 }
--- src/main/java/br/com/api/techvisit/customer/CustomerRepository.java
+++ src/main/java/br/com/api/techvisit/customer/CustomerRepository.java
@@ -1,5 +1,7 @@
 package br.com.api.techvisit.customer;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +11,6 @@ import br.com.api.techvisit.customer.model.CustomerModel;
 
 public interface CustomerRepository extends JpaRepository<CustomerModel, Long> {
 
-	CustomerModel findAllByOrganizationId(Long organizationId);
+	List<CustomerModel> findAllByOrganizationId(Long organizationId);
 
 }

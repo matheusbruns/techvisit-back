ac06269c25d1592a3dbc9f731833cbb6f1c8fc79
matheusbruns <matheus.rbruns@gmail.com>
2024-10-07 20:49:45 -0300

    TECH-17 feat: implementação dos testes

---
+++ src/test/java/br/com/techvisit/api/technician/TechnicianTest.java
@@ -0,0 +1,55 @@
+package br.com.techvisit.api.technician;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.*;
+
+import org.junit.jupiter.api.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import br.com.api.techvisit.organization.OrganizationService;
+import br.com.api.techvisit.technician.TechnicianRepository;
+import br.com.api.techvisit.technician.TechnicianService;
+import br.com.api.techvisit.technician.definition.TechnicianDTO;
+import br.com.api.techvisit.technician.definition.TechnicianModel;
+import br.com.api.techvisit.user.UserService;
+
+class TechnicianTest {
+
+	@Mock
+	private TechnicianRepository technicianRepository;
+
+	@Mock
+	private OrganizationService organizationService;
+
+	@Mock
+	private UserService userService;
+
+	@InjectMocks
+	private TechnicianService technicianService;
+
+	@BeforeEach
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test
+	void testGetAllTechnicians() {
+		Long organizationId = 1L;
+
+		List<TechnicianModel> technicians = Arrays.asList(new TechnicianModel(),
+				new TechnicianModel());
+
+		when(technicianRepository.findAllByOrganizationId(organizationId)).thenReturn(technicians);
+
+		List<TechnicianDTO> result = technicianService.getAll(organizationId);
+
+		assertNotNull(result);
+		assertEquals(2, result.size());
+		verify(technicianRepository, times(1)).findAllByOrganizationId(organizationId);
+	}
+
+}
